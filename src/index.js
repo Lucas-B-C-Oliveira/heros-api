1dc818bacc504808bbf08558cbebbc8e54d3b148
Lucas B C Oliveira <lucasbcoliveira189@protonmail.com>
2021-09-23 19:29:34 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const MongoDB = require('./db/strategies/mongodb')
 const Postgres = require('./db/strategies/postgres')
 
 
-
 const contextMongo = new ContextStrategy(new MongoDB())
 contextMongo.create()
 
